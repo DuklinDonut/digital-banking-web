3214fe488ea25fcda3f80ad5c0438a6fee094bd2
DuklinDonut <majidi_fatimaezzahra@emsi-edu.ma>
2022-06-26 00:55:01 +0000

    Initial commit

---
+++ src/app/new-customer/new-customer.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {Customer} from "../model/customer.model";
+import {CustomerService} from "../services/customer.service";
+
+@Component({
+  selector: 'app-new-customer',
+  templateUrl: './new-customer.component.html',
+  styleUrls: ['./new-customer.component.css']
+})
+export class NewCustomerComponent implements OnInit {
+  newCustomerFormGroup! : FormGroup;
+  constructor(private fb : FormBuilder, private customerService:CustomerService) {}
+
+  ngOnInit(): void {
+    this.newCustomerFormGroup=this.fb.group({
+      name : this.fb.control(null),
+      email: this.fb.control(null)
+    });
+  }
+
+  handleSaveCustomer() {
+    let customer:Customer=this.newCustomerFormGroup.value;
+    this.customerService.saveCustomers(customer).subscribe({
+      next : data=>{
+        alert("Customer has been successfully saved!");
+      },
+      error : err => {
+        console.log(err);
+      }
+    })
+  }
+}
